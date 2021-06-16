e680832deb6e5e5bed72c48dac67c8eba4bb0829
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-06-16 15:37:03 +0300

    brand-partners section added

---
+++ src/Components/OurBrands/Partners.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Partners.scss';
+export default function Partners() {
+    return (
+       <>
+            <div className="partners">
+                <div className="partners_wrapper">
+                    <div className="card " style={{width:"10rem", height:"10rem"}}>
+                        <img src="https://www.festisite.com/static/partylogo/img/logos/nike.png" className="card-img-top" alt="brandImage" />
+                        <div className="card-body">
+                            <p>36 products</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+       </>
+    )
+}
---
+++ src/Components/OurBrands/PartnersList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Partners from './Partners'
+
+export default function PartnersList() {
+    return (
+        <>
+            <div className="col-lg-8 mx-auto">
+                <h2>Our Brands</h2>
+                <Partners />
+                <Partners />
+                <Partners />
+                <Partners />
+            </div>
+        </>
+    )
+}
