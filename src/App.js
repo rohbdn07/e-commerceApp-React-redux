5a1579e8d70bf9faef5ae42fd33f321ba6e39ddd
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-06-19 12:09:24 +0300

    Switch routes are added for other pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.scss';
 import Homepage from './Pages/Homepage';
+import ShoppingCart from './Pages/ShoppingCart';
 
 
 function App() {
   return (
     <BrowserRouter>
-        <Homepage />
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route path="/shopping-cart">
+            <ShoppingCart />
+          </Route>
+        </Switch>
     </BrowserRouter>
         
   );
