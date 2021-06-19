d3f689d99969d2fb86401b97e362942a391590dd
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-06-19 12:07:40 +0300

    cart header component created

---
+++ src/Components/CartHeader/CartHeader.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './CartHeader.scss';
+
+export default function CartHeader() {
+    return (
+        <>
+            <div className="cartheader">
+                <div className="col-lg-8 mx-auto px-5">
+                    <h2>Shopping cart</h2>
+                </div>
+            </div>
+        </>
+    )
+}
