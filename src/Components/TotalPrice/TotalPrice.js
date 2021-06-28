73211604bd33cceae8c020455fd85e2ceb331a6f
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-06-28 18:58:39 +0300

    total price component is added to shopping cart page

---
+++ src/Components/TotalPrice/TotalPrice.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TotalPrice() {
+    return (
+       <>
+              <div className="totalprice">
+                <div className="totalprice_wrapper">
+                    <p>Have coupon?</p>
+                </div>
+            </div>
+       </>
+    )
+}
