9732a6e371e97f52fee93746a2f7901343e4cb1a
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-12 01:43:03 +0300

    axios added, trying to call another host where backend situated

--- src/Components/MakePurchase/Purchase.js
+++ src/Components/MakePurchase/Purchase.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { GrPrevious, GrNext } from 'react-icons/gr';
-import { FaTruck } from 'react-icons/fa';
-
-
-import './purchase.scss';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { GrPrevious, GrNext } from "react-icons/gr";
+import { FaTruck } from "react-icons/fa";
+import axiosInstance from "../../servicesAPI/axios";
+import "./purchase.scss";
 
 export default function Purchase() {
-    return (
-        <>
-            <div className="purchase">
-                <div className="purchase_wrapper">
-                    <div className="purchase_left">
-                      <Link to="/"><button className="btn btn-outline-secondary"><span><GrPrevious /></span>Continue shopping</button></Link> 
-                    </div>
-                    <div className="purchase_right">
-                    <button className="btn btn-primary">Checkout<span><GrNext className="nexticon" /></span></button>
-                    </div>
-                </div>
-                <div className="alert purchase_delievery">
-                    <p><span><FaTruck /></span>Free Delivery within 1-2 weeks</p>
-                </div>
-            </div>
-        </>
-    )
+  const { selectedItems } = useSelector((state) => state.productReducer);
+
+  const checkout = () => {
+    axiosInstance
+      .get("/create-checkout-session", {
+       selectedItems
+      })
+      .then(function (response) {
+        console.log("the response from backend", response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  return (
+    <>
+      <div className="purchase">
+        <div className="purchase_wrapper">
+          <div className="purchase_left">
+            <Link to="/">
+              <button className="btn btn-outline-secondary">
+                <span>
+                  <GrPrevious />
+                </span>
+                Continue shopping
+              </button>
+            </Link>
+          </div>
+          <div className="purchase_right">
+            <button onClick={checkout} className="btn btn-primary">
+              Checkout
+              <span>
+                <GrNext className="nexticon" />
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="alert purchase_delievery">
+          <p>
+            <span>
+              <FaTruck />
+            </span>
+            Free Delivery within 1-2 weeks
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/Components/ProductsContainer/ElectronicProducts.js
+++ src/Components/ProductsContainer/ElectronicProducts.js
@@ -1,47 +1,53 @@
-import React, {useState} from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectToCartAction } from '../../Redux/Action/selectToCartAction';
-import './Product.scss';
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectToCartAction } from "../../Redux/Action/selectToCartAction";
+import "./Product.scss";
 
 export default function ElectronicProducts() {
-    const {allProducts, selectedItems} = useSelector(state => state.productReducer);
-    const dispatch = useDispatch();
+  const { allProducts, selectedItems, isCartItem } = useSelector(
+    (state) => state.productReducer
+  );
+  const dispatch = useDispatch();
 
-    const addTocart = (itemId) => {
-        dispatch(selectToCartAction(itemId));
-        localStorage.setItem("selectedProduct", JSON.stringify(selectedItems))
-    }
-   
-    return (
-        <>
-              <h2 style={{marginTop:"3rem"}}>Electronics:</h2>
+  const addTocart = (itemId) => {
+    dispatch(selectToCartAction(itemId));
+  };
 
-        {
-            allProducts === false ? <h3>Couldn't able to get data from source</h3> : 
-            <div className="products">
-              
-                {
-                    allProducts.map((item)=>{
-                            return  (item.category === "electronics" ?
-                                    <div className="card col-lg-3 col-md-3 product_wrapper" key={item.id}>
-                                        <img src={item.image} className="card-img-top" alt="men's-product" />
-                                        <div className="card-body">
-                                        <h5 className="card-title">{item.title}</h5>
-                                        <p className="card-text price_text">${item.price}</p>
-                                        {/* <p className="card-text">{item.description}</p> */}
-                                        <button className="btn btn-primary" onClick={()=> addTocart(item.id)}>Add to cart</button>
-                                        </div>
-                                    </div>
+  return (
+    <>
+      <h2 style={{ marginTop: "3rem" }}>Electronics:</h2>
 
-                                : null)
-                            
-                    })
-                }
-            
-            </div>
-        }
-           
-        </>
-    )
+      {allProducts === false ? (
+        <h3>Couldn't able to get data from source</h3>
+      ) : (
+        <div className="products">
+          {allProducts.map((item) => {
+            return item.category === "electronics" ? (
+              <div
+                className="card col-lg-3 col-md-3 product_wrapper"
+                key={item.id}
+              >
+                <img
+                  src={item.image}
+                  className="card-img-top"
+                  alt="men's-product"
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{item.title}</h5>
+                  <p className="card-text price_text">${item.price}</p>
+                  {/* <p className="card-text">{item.description}</p> */}
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => addTocart(item.id)}
+                  >
+                    Add to cart
+                  </button>
+                </div>
+              </div>
+            ) : null;
+          })}
+        </div>
+      )}
+    </>
+  );
 }
