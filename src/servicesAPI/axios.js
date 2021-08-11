caabe8b6b417fc04c15fcd60ec5bf52cb4c587c5
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-12 01:40:27 +0300

    API call is addded seperately in service folder

---
+++ src/servicesAPI/axios.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:5050",
+  headers: {
+    "Content-type": "application/json",
+  },
+});
+export default axiosInstance;
