ec28c8ed238b6ba9e376c1eacaa25408e7659265
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-11 22:16:35 +0300

    route folder added

---
+++ backend-node/router/getPayment-route.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const app = express();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+//GET: route(/) to get all the data from db.
+router.get("/", (req, res) => {
+    res.send("hi from server");
+});
+
+router.post("/create-checkout-session", async(req, res) => {
+    const session = await stripe.checkout.session.create({
+        payment_method_types: ["card"],
+        line_items: [{
+            price_data: {
+                currency: 'usd',
+                product_data: req.body.map((item) => {
+                    return {
+                        name: item.productName,
+                        unit_amount: item.price * 1000,
+                    }
+                }),
+                unit_amount: 2000,
+            },
+            quantity: 1,
+        }, ],
+        mode: "payment",
+        succss_url: "http://localhost:4242/success.html",
+        cancel_url: "http://localhost:4242/cancel.html",
+    });
+});
+
+module.exports = router;
