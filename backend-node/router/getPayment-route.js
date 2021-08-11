fcf9fe5277973be98511ed5fb4f42d51c8d7ab22
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-11 22:24:05 +0300

    try..catch added

--- backend-node/router/getPayment-route.js
+++ backend-node/router/getPayment-route.js
@@ -10,25 +10,29 @@ router.get("/", (req, res) => {
 });
 
 router.post("/create-checkout-session", async(req, res) => {
-    const session = await stripe.checkout.session.create({
-        payment_method_types: ["card"],
-        line_items: [{
-            price_data: {
-                currency: 'usd',
-                product_data: req.body.map((item) => {
-                    return {
-                        name: item.productName,
-                        unit_amount: item.price * 1000,
-                    }
-                }),
-                unit_amount: 2000,
-            },
-            quantity: 1,
-        }, ],
-        mode: "payment",
-        succss_url: "http://localhost:4242/success.html",
-        cancel_url: "http://localhost:4242/cancel.html",
-    });
+    try {
+        const session = await stripe.checkout.session.create({
+            payment_method_types: ["card"],
+            line_items: [{
+                price_data: {
+                    currency: 'usd',
+                    product_data: req.body.map((item) => {
+                        return {
+                            name: item.productName,
+                            unit_amount: item.Totalprice * 1000,
+                        }
+                    }),
+                    unit_amount: 2000,
+                },
+                quantity: 1,
+            }, ],
+            mode: "payment",
+            succss_url: "http://localhost:4242/success.html",
+            cancel_url: "http://localhost:4242/cancel.html",
+        });
+    } catch (error) {
+        console.log('there is an error connection to Stripe', error)
+    }
 });
 
 module.exports = router;
