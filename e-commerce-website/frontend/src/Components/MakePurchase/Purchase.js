45034020612e3683bce89f32956253c1e158aac7
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-14 00:46:07 +0300

    getting correct response from backend(stripe)

--- e-commerce-website/frontend/src/Components/MakePurchase/Purchase.js
+++ e-commerce-website/frontend/src/Components/MakePurchase/Purchase.js
@@ -1,26 +1,30 @@
 import React from "react";
+import { useHistory, useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { GrPrevious, GrNext } from "react-icons/gr";
 import { FaTruck } from "react-icons/fa";
-import axiosInstance from "../../servicesAPI/axios";
+import { axiosInstance } from "../../servicesAPI/axios";
 import "./purchase.scss";
 
 export default function Purchase() {
   const { selectedItems } = useSelector((state) => state.productReducer);
 
-  const checkout = () => {
-    axiosInstance
-      .post("/api/create-checkout-session", {
-        productItems: selectedItems,
-      })
-      .then(function (response) {
-        console.log("the data saved to server", response.data);
-      })
-      .catch(function (error) {
-        console.log("there is an error", error);
-      });
+  const checkout = async () => {
+    try {
+      const { data } = await axiosInstance.post("/api/create-checkout-session", { dataFromClient: selectedItems });
+
+      console.log("the data saved to server", data.url);
+      console.log("the session data are", data.session);
+      if (data.url) {
+        window.location = data.url
+      }
+    } catch (error) {
+      console.log("there is an error", error);
+      alert('there is an error')  //TODO: change this to react tostify to alert the User
+    }
   };
+
   return (
     <>
       <div className="purchase">
