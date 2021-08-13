6cf67af10c7592d019a7f15e16f006b28fd715cd
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-13 12:24:53 +0300

    trying to fetching response

--- src/servicesAPI/axios.js
+++ src/servicesAPI/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5050",
   headers: {
-    "Content-type": "application/json",
+    "Content-Type": "application/json",
   },
 });
 export default axiosInstance;
