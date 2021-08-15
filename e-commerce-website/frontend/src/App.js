fa6c164631bd4673c64837bd14b9ea5ca0d4729f
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-15 12:49:50 +0300

    some styling and grid changes

--- e-commerce-website/frontend/src/App.js
+++ e-commerce-website/frontend/src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import './App.scss';
-import Homepage from './Pages/Homepage';
-import ShoppingCart from './Pages/ShoppingCart';
-
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import "./App.scss";
+import Success from "./Components/SuccessPayment/Success";
+import Homepage from "./Pages/Homepage";
+import ShoppingCart from "./Pages/ShoppingCart";
 
 function App() {
   return (
     <BrowserRouter>
-        <Switch>
-          <Route exact path="/">
-            <Homepage />
-          </Route>
-          <Route path="/shopping-cart">
-            <ShoppingCart />
-          </Route>
-        </Switch>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path="/shopping-cart">
+          <ShoppingCart />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+      </Switch>
     </BrowserRouter>
-        
   );
 }
 
