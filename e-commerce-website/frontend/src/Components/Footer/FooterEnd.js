72b59e33e011414963e044db1e5cf65f42c2678f
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-15 15:45:22 +0300

    styling the footer section

--- e-commerce-website/frontend/src/Components/Footer/FooterEnd.js
+++ e-commerce-website/frontend/src/Components/Footer/FooterEnd.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import { FaCcVisa, FaCcPaypal, FaCcMastercard } from 'react-icons/fa';
-
+import React from "react";
+import { FaCcVisa, FaCcPaypal, FaCcMastercard } from "react-icons/fa";
 
 export default function FooterEnd() {
-    return (
-       <>
-            <div className="footer_end">
-                <div className="fotend_wrapper">
-                    <div className="fotend_left">
-                        <p className="text-muted">2021 RB-Cart</p>
-                    </div>
-                    <div className="fotend_middle text-muted">
-                        <p>rohit.raj.bhandari07@gmail.com</p>
-                        <p>+358449702512</p>
-                        <p>Helsinki-Finland</p>
-                    </div>
-                    <div className="fotend_cards text-muted">
-                        <FaCcVisa className="mx-1"/>
-                        <FaCcPaypal className="mx-2" />
-                        <FaCcMastercard />
-                    </div>
-
-                </div>
-
-            </div>
-       </>
-    )
+  return (
+    <>
+      <div className="footer_end">
+        <div className="fotend_wrapper">
+          <div className="fotend_left">
+            <p className="text-muted">2021 RB-Cart</p>
+          </div>
+          <div className="fotend_middle text-muted">
+            <p>rohit.raj.bhandari07@gmail.com</p>
+            <p>+358449702512</p>
+            <p>Helsinki-Finland</p>
+          </div>
+          <div className="fotend_cards text-muted">
+            <FaCcVisa className="mx-1" />
+            <FaCcPaypal className="mx-2" />
+            <FaCcMastercard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
