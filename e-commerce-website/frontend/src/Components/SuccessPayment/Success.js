f195b2acf3df73f8feebe4da8a4fcc465a776b43
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-15 12:48:43 +0300

    success page with component added

---
+++ e-commerce-website/frontend/src/Components/SuccessPayment/Success.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Topbar from "../Topbar/Topbar";
+
+export default function Success() {
+  return (
+    <>
+      <Topbar />
+      <div>
+        <h3>Thank you for your purchase</h3>
+        <h6>Please check your email. We shortly send you the recipet.</h6>
+      </div>
+    </>
+  );
+}
---
+++ e-commerce-website/frontend/src/Pages/SuccessPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Success from "../Components/SuccessPayment/Success";
+
+export default function SuccessPage() {
+  return (
+    <div>
+      <Success />
+    </div>
+  );
+}
