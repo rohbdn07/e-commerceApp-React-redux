92807e106cfe201b027b52483add50dce4206350
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-16 14:13:33 +0300

    line intent is corrected

--- e-commerce-website/frontend/src/Components/Discount/Discount.js
+++ e-commerce-website/frontend/src/Components/Discount/Discount.js
@@ -1,21 +1,30 @@
-import React from 'react';
-import './Discount.scss';
+import React, { useState } from "react";
+import "./Discount.scss";
 
 export default function Discount() {
-    return (
-       <>
-            <div className="discount">
-                <div className="discount_wrapper">
-                    <p>Have coupon?</p>
-                    <div className="search_input">
-                            <form className="d-flex">
-                                <input class="form-control me-1" type="search" placeholder="Enter coupon code..." aria-label="Search" />
-                                <button class="btn btn-outline-primary" type="submit">Apply</button>
-                            </form>
-                        </div>
-
-                </div>
-            </div>
-       </>
-    )
+  return (
+    <>
+      <div className="discount">
+        <div className="discount_wrapper">
+          <p>Have coupon?</p>
+          <div className="search_input">
+            <form className="d-flex">
+              <input
+                class="form-control me-1"
+                type="search"
+                placeholder="Enter coupon code..."
+                aria-label="Search"
+              />
+              <button class="btn btn-outline-primary" type="submit">
+                Apply
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="get_discount">
+          <p>Get discount only above 100 $</p>
+        </div>
+      </div>
+    </>
+  );
 }
