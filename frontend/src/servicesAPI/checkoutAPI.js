44ad33ac72810fd99dfda44c2432a259a3d67941
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-08-21 11:36:03 +0300

    use of env seperately

--- frontend/src/servicesAPI/checkoutAPI.js
+++ frontend/src/servicesAPI/checkoutAPI.js
@@ -1,23 +1,26 @@
 import { axiosInstance } from "./axios";
 
 export const checkoutAPI = async (selectedItems, setLoading) => {
-  try {
-    setLoading(true);
-    const { data } = await axiosInstance.post("/api/create-checkout-session", {
-      dataFromClient: selectedItems,
-    });
-    setLoading(false);
-    
-    console.log("the data saved to server", data.url);
-    console.log("the session data are", data.session);
-    console.log("the session data are", data.coupon);
+   try {
+      setLoading(true);
+      const { data } = await axiosInstance.post(
+         "/api/create-checkout-session",
+         {
+            dataFromClient: selectedItems,
+         }
+      );
+      setLoading(false);
 
-    if (data && data.url) {
-      window.location = data.url;
-    }
-    localStorage.removeItem("selectedProduct");
-  } catch (error) {
-    console.log("there is an error", error);
-    alert("there is an error", error); //TODO: change this to react tostify to alert the User
-  }
+      // console.log("the data saved to server", data.url);
+      // console.log("the session data are", data.session);
+      // console.log("the session data are", data.coupon);
+
+      if (data && data.url) {
+         window.location = data.url;
+      }
+      localStorage.removeItem("selectedProduct");
+   } catch (error) {
+      console.log("there is an error", error);
+      alert("there is an error", error); //TODO: change this to react tostify to alert the User
+   }
 };
