e74efe374c2539c378b49ed8a17f0dd792e500fe
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-09-29 14:04:09 +0300

    button disabled after item is added to cart list

--- frontend/src/utils/Button/AddToCartButton.tsx
+++ frontend/src/utils/Button/AddToCartButton.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectToCartAction } from "../../Redux/Action/selectToCartAction";
 import { RootState } from "../../Redux/Reducer";
@@ -5,17 +6,28 @@ import { RootState } from "../../Redux/Reducer";
 export default function AddToCartButton(props: any): JSX.Element {
    const dispatch = useDispatch();
    const { itemId } = props;
-   const { isAdded }: any = useSelector(
-      (state: RootState) => state.productReducer
+   const selectedItems: any = useSelector(
+      (state: RootState) => state.productReducer?.selectedItems
    );
 
-   const addTocart = (itemId: string) => {
+   const isIdExisted = selectedItems.some((item: any) => {
+      if (item.id === itemId) {
+         return true;
+      }
+      return false;
+   });
+
+   const addTocart = (itemId: string): void => {
       dispatch(selectToCartAction(itemId));
    };
    return (
       <>
-         <button className="btn btn-primary" onClick={() => addTocart(itemId)}>
-            {!isAdded ? "Add to cart" : "Added"}
+         <button
+            className="btn btn-primary"
+            onClick={() => addTocart(itemId)}
+            disabled={isIdExisted}
+         >
+            {isIdExisted ? "Added" : "Add to cart"}
          </button>
       </>
    );
