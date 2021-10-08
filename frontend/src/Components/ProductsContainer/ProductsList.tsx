3124d1e68ecce5adb5a48ebc74d5a5eafb367bec
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-10-08 16:12:37 +0300

    refactor code

--- frontend/src/Components/ProductsContainer/ProductsList.tsx
+++ frontend/src/Components/ProductsContainer/ProductsList.tsx
@@ -1,15 +1,42 @@
 import React from "react";
 import Products from "./Products";
 
+interface IText {
+   title: string;
+   category: string;
+}
+
+const text: IText[] = [
+   {
+      title: "Men's clothes",
+      category: "men's clothing",
+   },
+   {
+      title: "Women's clothes",
+      category: "women's clothing",
+   },
+   {
+      title: "Electronics",
+      category: "electronics",
+   },
+   {
+      title: "Jewelery",
+      category: "jewelery",
+   },
+];
+
 export default function ProductsList(): JSX.Element {
    return (
       <>
          <div className="container">
             <div className="col-lg-12 product_list">
-               <Products title="Men's clothes" category="men's clothing" />
-               <Products title="Women's clothes" category="women's clothing" />
-               <Products title="Electronics" category="electronics" />
-               <Products title="Jewelery" category="jewelery" />
+               {text?.map((item, index) => (
+                  <Products
+                     key={index}
+                     title={item.title}
+                     category={item.category}
+                  />
+               ))}
             </div>
          </div>
       </>
