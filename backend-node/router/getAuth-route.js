b3ee9f44fbafaee3ee4a0026c356ad91165a8376
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-10-24 14:29:55 +0300

    mongodb for database

--- backend-node/router/getAuth-route.js
+++ backend-node/router/getAuth-route.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const app = express();
 
-router.get("/api/login", (req, res) => {
+router.post("/api/login", (req, res) => {
+   const { username, email, password } = req.body;
    res.status(200).json({ message: "login successfull" });
 });
 
