e524bce92ff18eb1dd8e706643038b4836e9e20b
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-10-30 01:54:42 +0300

    defined Types and added register reducer

--- frontend/src/Redux/Reducer/index.ts
+++ frontend/src/Redux/Reducer/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import productReducer from "./productReducer";
 import detailPageBanner from "./detailPageBanner";
+import registerReducer from "./AuthReducer/registerReducer";
 
 const rootReducer = combineReducers({
    productReducer,
    detailPageBanner,
+   registerReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
