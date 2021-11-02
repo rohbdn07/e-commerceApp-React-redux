0358277d360fe10bee216801ab16c7ba4927fdd8
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-02 03:24:33 +0200

    moved into routes folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,8 @@
-import React from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
-import SuccessPage from "./Pages/SuccessPage";
-import CancelledPage from "./Pages/CancelledPage";
-import Homepage from "./Pages/Homepage";
-import ShoppingCart from "./Pages/ShoppingCart";
-import ProductDetailsPage from "./Pages/ProductDetailsPage";
-import ScrollToTop from "./Components/ScrollToTop";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
-import CategoryPage from "./Pages/CategoryPage";
+import AppRoutes from "./Routes";
 
 // Create a client
 const queryClient = new QueryClient();
@@ -18,28 +11,7 @@ function App(): JSX.Element {
    return (
       <QueryClientProvider client={queryClient}>
          <BrowserRouter>
-            <ScrollToTop />
-            <Switch>
-               <Route exact path="/">
-                  <Homepage />
-               </Route>
-               <Route path="/category/:categoryname">
-                  <CategoryPage />
-               </Route>
-               <Route path="/product/:categoryname/:id">
-                  <ProductDetailsPage />
-               </Route>
-               <Route path="/shopping-cart">
-                  <ShoppingCart />
-               </Route>
-               <Route path="/success">
-                  <SuccessPage />
-               </Route>
-               <Route path="/cancelled">
-                  <CancelledPage />
-               </Route>
-               <Redirect from="*" to="/" />
-            </Switch>
+            <AppRoutes />
          </BrowserRouter>
          <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
