2663717f4296e22cef047274ccdccbec1555ba37
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-06 00:23:50 +0200

    interfaces types for login and register responses

--- frontend/src/interfaces/userAuth.interface.ts
+++ frontend/src/interfaces/userAuth.interface.ts
@@ -1,8 +1,35 @@
-interface IUserAuth {
+export interface IUserAuth {
     email: string;
     password: string;
     // confirmPassword?: string;
     userName?: string;
 }
 
-export default IUserAuth;
+export interface IRegisterUserResponse {
+    user: {
+        data: {
+            token?: string
+            message: string
+            success: boolean
+            userName?: string
+        }
+
+    }
+}
+
+export interface ILoginUserResponse {
+    data: {
+        message: string
+        success: boolean
+        token: string
+        userName?: string
+    }
+    status?: number
+    statusText?: string
+
+}
+
+export interface ILoginState {
+    user: ILoginUserResponse
+}
+
