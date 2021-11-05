8817e635d8fa89c5e7834ab0e3c3ac15073e0216
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-06 00:17:40 +0200

    use of api service, type defined, dispatch register action

--- frontend/src/Redux/Action/Auth/registerUserAction.ts
+++ frontend/src/Redux/Action/Auth/registerUserAction.ts
@@ -1,60 +1,57 @@
-
 import { Dispatch } from 'redux';
-import IUserAuth from "../../../interfaces/userAuth.interface";
-import { axiosFetchAuthAPI } from "../../../servicesAPI/axios";
+import { IRegisterUserResponse, IUserAuth } from "../../../interfaces/userAuth.interface";
+import { userService } from '../../../servicesAPI/Auth-service/Users/UserService';
 import { Action } from '../actionInterface';
 import { ActionType } from '../actionTypes';
 
 
-type UserInfo = {
-    data: IUserAuth;
-};
-
 /**
  * 
- * @param userCredentials email, password, username
+ * @param userCredentials email, password, username.
+ * @userService register function which fetch register API.
  * @returns void
  */
-
 export const registerUserAction = (userCredentials: IUserAuth) => async (dispatch: Dispatch<Action>) => {
-    try {
-        if (typeof userCredentials !== undefined || null) {
-            const { data }: UserInfo = await axiosFetchAuthAPI.post('/api/register', userCredentials);
-            dispatch({
-                type: ActionType.REGISTER_USER,
-                payload: data,
-            })
-        }
-    } catch (error) {
-        dispatch({
-            type: ActionType.PRODUCT_LOADING_ERROR
-        });
 
-        console.log('there is error', error)
+    if (userCredentials && typeof userCredentials !== undefined) {
+        try {
+            const registerResponse: IRegisterUserResponse | undefined = await userService.register(userCredentials);
+            // console.log('the register response', registerResponse)
+            if (registerResponse !== undefined) {
+                dispatch({
+                    type: ActionType.USER_REGISTER_SUCCESS,
+                    payload: registerResponse,
+                })
+            }
+        }
+        catch (error) {
+            dispatch({
+                type: ActionType.USER_REGISTER_FAILURE
+            });
+            // console.log('there is error', error)
+        }
+
     }
 }
 
 
 /**
- * @function cancelRegisterForm 
- * cancel the response message (got from backend)
+ * @description delete the response message (got from backend)
  * in order to remove Alert bar.
+ * @function cancelRegisterForm 
  * @returns void
  */
-
 export const cancelRegisterForm = () => async (dispatch: Dispatch<Action>) => {
     try {
         dispatch({
             type: ActionType.CANCEL_REGISTER_FORM,
-
         })
-
     } catch (error) {
         dispatch({
-            type: ActionType.PRODUCT_LOADING_ERROR
+            type: ActionType.USER_REGISTER_FAILURE
         });
 
-        console.log('there is error', error)
+        // console.log('there is error', error)
     }
 }
 
