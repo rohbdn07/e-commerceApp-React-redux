b80a93de42f099de021c518bbaea48f8b1472090
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-06 00:11:31 +0200

    register user

--- frontend/src/Components/Auth/Register/Register.tsx
+++ frontend/src/Components/Auth/Register/Register.tsx
@@ -12,45 +12,49 @@ import {
    cancelRegisterForm,
    registerUserAction,
 } from "../../../Redux/Action/Auth/registerUserAction";
-import IUserAuth from "../../../interfaces/userAuth.interface";
-import AlertInfo from "./AlertInfo";
+import {
+   IRegisterUserResponse,
+   IUserAuth,
+} from "../../../interfaces/userAuth.interface";
+import AlertInfo from "../AlertInfo";
 import { RootState } from "../../../Redux/Reducer";
 
-interface IRegisterUserResponse {
-   registerdUserResponse: {
-      message: string;
-   };
-}
+type RegisterProps = {
+   open: boolean;
+   toogleLogin: () => void;
+   setOpen: (open: boolean) => void;
+};
 
 /**
- *@function Register use for registration the new user.
+ * @description renders the register form
+ * for registration the new user.
+ *@function Register 
+   *@param {boolean} open - open dialog.
+   @param {RegisterProps} toogleLogin - toogle login dialog.
+   @param {RegisterProps} setOpen - set open dialog.
  * @returns JSX element
  */
-
-export default function Register() {
-   const authResponse: IRegisterUserResponse = useSelector(
+export default function Register({
+   toogleLogin,
+   open,
+   setOpen,
+}: RegisterProps): JSX.Element {
+   const registerResponse: IRegisterUserResponse = useSelector(
       (state: RootState) => state.registerReducer
    );
 
    const dispatch = useDispatch();
 
-   const [alertInfo, setAlertInfo] = React.useState("");
+   const [formError, setFormError] = React.useState(false);
 
-   const [open, setOpen] = React.useState(false);
-
-   //User input credentials
    const [userCredentials, setUserCredentials] = React.useState<IUserAuth>({
       userName: "",
       email: "",
       password: "",
    });
 
+   //Handle user input
    const { userName, email, password } = userCredentials;
-
-   //Open register model form
-   const handleClickOpen = (): void => {
-      setOpen(true);
-   };
 
    /**
     * @function handleClose reset alert messages, user's credentials
@@ -58,14 +62,19 @@ export default function Register() {
     * in order to remove Alert bar.
     */
    const handleClose = (): void => {
-      setAlertInfo("");
+      setFormError(false);
       resetCredentials();
       setOpen(false);
-      if (authResponse.registerdUserResponse.message) {
+      if (registerResponse.user.data?.message) {
          dispatch(cancelRegisterForm());
       }
    };
 
+   /**
+    * @function resetCredentials reset the user's credentials
+    * update the state with empty values.
+    * @return void
+    */
    const resetCredentials = () => {
       setUserCredentials({
          userName: "",
@@ -74,94 +83,106 @@ export default function Register() {
       });
    };
 
+   /**
+    * @function handleChange handle the user input
+    * @param event
+    */
    const handleChange = (event: React.FormEvent): void => {
-      // const target = event.target as HTMLSelectElement
+      event.preventDefault();
       const { name, value } = event.target as HTMLSelectElement;
       setUserCredentials({ ...userCredentials, [name]: value });
    };
 
    /**
-    * @function handleSubmit send the user's credentials.
+    * @function handleSubmit send the user's credentials for registration.
     * @dispatch user's credentials as an action creator.
     * @param event React.FormEvent
     * @returns void
     */
    const handleSubmit = (event: React.FormEvent): void => {
       event.preventDefault();
-      if (
-         userCredentials.email &&
-         userCredentials.password &&
-         userCredentials.userName
-      ) {
+      if (email && password && userName) {
          dispatch(registerUserAction(userCredentials));
-         resetCredentials();
+         setFormError(false);
       } else {
-         setAlertInfo("Please enter your credentials");
+         setFormError(true);
+      }
+      resetCredentials();
+      if (registerResponse.user.data?.success) {
+         setOpen(false);
       }
    };
 
    return (
-      <div>
-         <p onClick={handleClickOpen}>Register</p>
-         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>Register</DialogTitle>
-            {alertInfo && alertInfo ? (
-               <AlertInfo info={alertInfo} />
-            ) : (
-               <AlertInfo info={authResponse.registerdUserResponse?.message} />
-            )}
-
-            <DialogContent>
-               <DialogContentText>
-                  Please register with your email address and Password.
-               </DialogContentText>
-               <TextField
-                  autoFocus
-                  margin="dense"
-                  id="username"
-                  label="Username"
-                  type="text"
-                  fullWidth
-                  variant="standard"
-                  name={"userName"}
-                  value={userName}
-                  onChange={(e) => handleChange(e)}
-                  required={true}
-               />
-               <TextField
-                  autoFocus
-                  margin="dense"
-                  id="email"
-                  label="Email Address"
-                  type="email"
-                  fullWidth
-                  variant="standard"
-                  name={"email"}
-                  value={email}
-                  onChange={(e) => handleChange(e)}
-                  required={true}
-               />
-               <TextField
-                  autoFocus
-                  margin="dense"
-                  id="password"
-                  label="Password"
-                  type="password"
-                  fullWidth
-                  variant="standard"
-                  name={"password"}
-                  value={password}
-                  onChange={(e) => handleChange(e)}
-                  required={true}
-               />
-            </DialogContent>
-            <DialogActions>
-               <ButtonGroup variant="text" aria-label="text button group">
-                  <Button onClick={handleClose}>CANCEL</Button>
-                  <Button onClick={handleSubmit}>REGISTER</Button>
-               </ButtonGroup>
-            </DialogActions>
-         </Dialog>
-      </div>
+      <>
+         <div>
+            <Dialog open={open} onClose={handleClose}>
+               <DialogTitle>Register</DialogTitle>
+               {registerResponse.user.data?.message ? (
+                  <AlertInfo
+                     info={registerResponse.user.data?.message}
+                     status={registerResponse.user.data?.success}
+                  />
+               ) : null}
+               <DialogContent>
+                  <DialogContentText>
+                     Please register with your Username, Email and Password.
+                  </DialogContentText>
+                  <TextField
+                     error={formError}
+                     autoFocus
+                     margin="dense"
+                     id="username"
+                     label="Username"
+                     type="text"
+                     fullWidth
+                     variant="standard"
+                     name={"userName"}
+                     value={userName}
+                     onChange={handleChange}
+                     required={true}
+                  />
+                  <TextField
+                     error={formError}
+                     autoFocus
+                     margin="dense"
+                     id="email"
+                     label="Email Address"
+                     type="email"
+                     fullWidth
+                     variant="standard"
+                     name={"email"}
+                     value={email}
+                     onChange={handleChange}
+                     required={true}
+                  />
+                  <TextField
+                     error={formError}
+                     autoFocus
+                     margin="dense"
+                     id="password"
+                     label="Password"
+                     type="password"
+                     fullWidth
+                     variant="standard"
+                     name={"password"}
+                     value={password}
+                     onChange={handleChange}
+                     required={true}
+                  />
+               </DialogContent>
+               <DialogActions>
+                  <ButtonGroup variant="text" aria-label="text button group">
+                     <Button onClick={handleClose}>CANCEL</Button>
+                     <Button onClick={handleSubmit}>REGISTER</Button>
+                  </ButtonGroup>
+               </DialogActions>
+               <div className="text-center">
+                  <p>Already have an account? </p>
+                  <Button onClick={toogleLogin}>Login</Button>
+               </div>
+            </Dialog>
+         </div>
+      </>
    );
 }
