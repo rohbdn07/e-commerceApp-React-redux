595a057332a25440983bf516b426bee9873a23e1
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-11-23 03:01:46 +0200

    unnecessary code is commented

--- frontend/src/Components/Auth/LoginRegister/LoginRegister.tsx
+++ frontend/src/Components/Auth/LoginRegister/LoginRegister.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import {
-   ILoginState,
-   IRegisterUserResponse,
-} from "../../../interfaces/userAuth.interface";
+import { ILoginState } from "../../../interfaces/userAuth.interface";
 import { RootState } from "../../../Redux/Reducer";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
@@ -21,9 +18,9 @@ export default function LoginRegister(): JSX.Element {
 
    const [userName, setUserName] = React.useState("");
 
-   const registerUser: IRegisterUserResponse = useSelector(
-      (state: RootState) => state.registerReducer
-   );
+   // const registerUser: IRegisterUserResponse = useSelector(
+   //    (state: RootState) => state.registerReducer
+   // );
 
    const loginUser: ILoginState = useSelector(
       (state: RootState) => state.loginReducer
