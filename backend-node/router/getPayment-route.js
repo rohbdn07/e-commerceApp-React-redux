ab72bec67ef81c84a12a530a255c8202118ae9f6
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-12-18 14:16:29 +0200

    put some description as a comment

--- backend-node/router/getPayment-route.js
+++ backend-node/router/getPayment-route.js
@@ -2,12 +2,17 @@ const express = require("express");
 const {
    paymentController,
 } = require("../controllers/payment/payment-controller");
+const { verifyToken } = require("../middleware/auth/token/auth.token");
 const router = express.Router();
 const app = express();
 
 /**
  * POST: route(/api/create-checkout-session) to post product(s) data to stripe (payment gateway).
+ * @param route /api/create-checkout-session, is the route to post product(s) data to stripe (payment gateway).
+ * @param varifyToken verify token and return the decoded token.
+ * @param paymentController it contains data coming from client server.
+ * @returns res.
  */
-router.post("/api/create-checkout-session", paymentController);
+router.post("/api/create-checkout-session", verifyToken, paymentController);
 
 module.exports = router;
