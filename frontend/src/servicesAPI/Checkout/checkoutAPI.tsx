2ac65712a0f94c92bf3b2d970590de13f641ef50
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-12-18 14:25:19 +0200

    defined token type, set header with that token, axios post request

--- frontend/src/servicesAPI/Checkout/checkoutAPI.tsx
+++ frontend/src/servicesAPI/Checkout/checkoutAPI.tsx
@@ -1,6 +1,11 @@
+import axios from "axios";
 import React from "react";
 import { IProductItems } from "../../Components/CartItems/CartItems";
-import { axiosInstance } from "../axios";
+// import { axiosInstance } from "../axios";
+
+type userToken = {
+   token: string;
+};
 
 /**
  * Hook for redirecting to stipe payment page.
@@ -10,25 +15,62 @@ import { axiosInstance } from "../axios";
 
 export const useCheckoutAPI = (selectedItems: IProductItems) => {
    const [loading, setLoading] = React.useState(false);
+   const [errorMsg, setErrorMsg] = React.useState(false);
 
-   const callCheckoutAPI = async () => {
+   const callCheckoutAPI = async (userToken: userToken) => {
+      // set headers with token.
+      const config = {
+         headers: { "x-access-token": userToken },
+      };
+
       try {
          setLoading(true);
-         const { data } = await axiosInstance.post(
+         const { data } = await axios.post(
             "/api/create-checkout-session",
             {
                dataFromClient: selectedItems,
-            }
+            },
+            config
          );
          setLoading(false);
 
+         // if success redirect to stripe payment page
          if (data && data.url) {
             window.location = data.url;
          }
+
          localStorage.removeItem("selectedProduct");
       } catch (error) {
+         // catch the error and set loading to false
          console.log("there is an error", error);
+         setLoading(false);
+         setErrorMsg(true);
       }
    };
-   return [loading, callCheckoutAPI] as const;
+
+   return [loading, errorMsg, callCheckoutAPI] as const;
 };
+
+// const callCheckoutAPI = async (userToken: any) => {
+//    try {
+//       setLoading(true);
+//       const { data } = await axiosInstance.post(
+//          "/api/create-checkout-session",
+//          {
+//             dataFromClient: selectedItems,
+//          }
+//       );
+//       setLoading(false);
+
+//       console.log("the api call for checkout response", data);
+
+//       if (data && data.url) {
+//          window.location = data.url;
+//       }
+//       localStorage.removeItem("selectedProduct");
+//    } catch (error) {
+//       console.log("there is an error", error);
+//       setLoading(false);
+//       setErrorMsg(true);
+//    }
+// };
