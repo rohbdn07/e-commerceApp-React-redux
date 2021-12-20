6872c74fc7e4b3e610c0b2912312c0aabc3a405c
rohbdn07 <rohit.raj.bhandari07@gmail.com>
2021-12-21 01:48:03 +0200

    code refactor, services created for auth

--- backend-node/controllers/user/user-controller.js
+++ backend-node/controllers/user/user-controller.js
@@ -1,78 +1,56 @@
-const { User } = require("../../database/models/user/user.auth");
-const validator = require("validator");
+const UserAuthService = require("../../services/user/auth.user.service");
 const validateRegisterInput = require("../../middleware/auth/validateRegisterInput");
-const {
-   bcryptThePasswordAndSaveUser,
-   comparePassword,
-} = require("./utils/bcrypt");
-const { jwtSignInUser, jwtSignUpUser } = require("./utils/jwtSignUser");
 
 /**
- * @param req it contains user's credentials.
- * @param res send response: jwt token, message, success to client server.
+ * @param req it contains user's credentials received from client.
+ * @param res send response object.
+ * @returns response {success, message} to client server.
  */
 const register = async (req, res) => {
    try {
       const { errors, isValid } = validateRegisterInput(req.body);
 
+      // return error message if email is not valid.
       if (errors.email) {
          return res.json({
             success: false,
-            message: errors.email, //return error message
+            message: errors.email,
          });
       }
 
+      // return error message if password is not valid.
       if (errors.password) {
          return res.json({
             success: false,
-            message: errors.password, //return error message
+            message: errors.password,
          });
       }
 
-      //check if the email is already exist in database.
-      const userExist = await User.findOne({ email: req.body.email });
+      //check if the email is already existed in database through UserAuthService.
+      const userExist = await UserAuthService.checkIfUserExist(req.body.email);
 
+      // return error message if the email is already existed in database.
       if (userExist) {
-         return res
-            .status(200)
-            .json({ message: "User with this email is already existed!" });
-      } else {
-         //if user doesn't exist, then create the user.
-         const newUser = new User({
-            userName: req.body.userName,
-            email: req.body.email,
-            password: req.body.password,
+         return res.status(200).json({
+            success: false,
+            message: "User with this email is already exists!",
          });
+      } else {
+         const { userName, email, password } = req.body; //get user's credentials.
 
-         //bcrypt the password into hash-password.
-         //then save the user into database.
-         //then send response to client server with JWT token.
-         const userIsSaved = await bcryptThePasswordAndSaveUser(newUser);
+         // register the user into database through UserAuthService.
+         const registeredUser = await UserAuthService.register(
+            userName,
+            email,
+            password
+         );
 
-         if (userIsSaved) {
-            const payload = {
-               id: userIsSaved._id,
-               email: userIsSaved.email,
-            };
-
-            const token = await jwtSignUpUser(payload); //generate the token.
-
-            if (token && token.length > 0) {
-               return res.json({
-                  success: true,
-                  token: "Bearer " + token,
-                  message: "User has been created. Please Login now!",
-               });
-            } else {
-               return res.json({
-                  success: false,
-                  message: "Something went wrong. Please try again!",
-               });
-            }
-         }
+         res.json(registeredUser); //send response to client server.
       }
    } catch (error) {
       console.log("Unable to register! Failed to register!!!", error);
+
+      // return error message if above code fails.
       res.status(400).json({
          success: false,
          message: "Register unsuccessful, please try with valid creadintials",
@@ -81,62 +59,32 @@ const register = async (req, res) => {
 };
 
 /**
- * @param req it contains user's login credentials.
- * @param res send response: jwt token, message, success to client server.
+ * @param req it contains user's credentials received from client.
+ * @param res send response object.
+ * @returns response {success, message, username} to client server.
  */
 const login = async (req, res) => {
    try {
-      //check if the email is already exist in database.
-      const existUser = await User.findOne({ email: req.body.email });
+      //check if the email is already existed in database through UserAuthService.
+      const userExist = await UserAuthService.checkIfUserExist(req.body.email);
 
-      if (!existUser) {
+      if (!userExist) {
          return res.json({ message: "This user doesn't exit!" });
       }
 
-      const passwordInputByUser = req.body.password;
-
-      //compare the password input by user with the password in database.
-      //if the password input by user is correct,
-      //then send response to client server with JWT token.
-      const isPasswordMatch = await comparePassword(
-         existUser,
-         passwordInputByUser
+      // login the user through UserAuthService.
+      const loggedIn = await UserAuthService.login(
+         req.body.email,
+         req.body.password
       );
 
-      if (!isPasswordMatch) {
-         return res.json({
-            message: "Email or Password is incorrect!!",
-            error: "Email or password is incorrect!!",
-         });
-      } else {
-         //if password matched, then generate the token.
-         //and send the token and username to user.
-         const userName = existUser.userName;
-         const existUserEmail = existUser.email;
-
-         const payload = {
-            id: existUser._id,
-            email: existUserEmail,
-            userName,
-         };
-
-         const token = await jwtSignInUser(payload); //generate the token.
-         if (token && token.length > 0) {
-            res.json({
-               success: true,
-               token: "Bearer " + token,
-               message: "You're loggedIn",
-               userName,
-            });
-         } else {
-            res.json({
-               message:
-                  "Login unsuccessful, please try with valid creadintials",
-            });
-         }
-      }
+      res.status(200).json(loggedIn); //send response to client server.
    } catch (error) {
       console.log("Error while login user", error);
+      res.json({
+         success: false,
+         message: "Login unsuccessful, please try with valid creadintials",
+      });
    }
 };
 
